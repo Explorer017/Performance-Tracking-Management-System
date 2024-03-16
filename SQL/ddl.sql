f783e2dd574991fff1d6d59301bab102489888d1
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2024-03-16 16:03:09 +0000

    replaced old ddl with ddl for new schema
    not finished yet, might mess around with how many tables there are

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -1,55 +1,126 @@
+-- mirosdb.`user` definition
 
-CREATE TABLE IF NOT EXISTS `MIROSdb`.`manager` (
-  `managerID` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `middle_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`managerID`));
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_access_level` int(11) NOT NULL,
+  `first_name` varchar(45) NOT NULL,
+  `middle_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `points` int(11) NOT NULL,
+  `higher_user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `higher_user_id` (`higher_user_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`higher_user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS `MIROSdb`.`supervisor` (
-  `supervisorID` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `middle_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `points` INT NOT NULL,
-  `managerID` INT,
-  PRIMARY KEY (`supervisorid`),
-  FOREIGN KEY (`managerID`)
-    REFERENCES `MIROSdb`.`Manager` (`managerID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
-    
-CREATE TABLE IF NOT EXISTS `MIROSdb`.`research_officer` (
-  `officerID` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NOT NULL,
-  `middle_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NULL,
-  `supervisorID` INT NULL,
-  `points` INT NULL,
-  PRIMARY KEY (`officerID`),
-  CONSTRAINT `supervisorID`
-    FOREIGN KEY (`supervisorID`)
-    REFERENCES `MIROSdb`.`supervisor` (`supervisorID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
-    
-CREATE TABLE IF NOT EXISTS `MIROSdb`.`Submission` (
-  `submissionID` INT NOT NULL,
-  `officerID` INT NULL,
-  `fileID` INT NULL,
-  `Section` VARCHAR(45) NULL,
-  `Item` VARCHAR(45) NULL,
-  `Date_Uploaded` DATE NULL,
-  `Approved` BOOLEAN NULL,
-  PRIMARY KEY (`submissionID`),
-  CONSTRAINT `officerID`
-    FOREIGN KEY (`officerID`)
-    REFERENCES `MIROSdb`.`research_officer` (`officerID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE);
+
+-- mirosdb.a6_professional_affilliations_memberships definition
+
+CREATE TABLE `a6_professional_affilliations_memberships` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `a6_professional_affilliations_memberships_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.b3_operational_development_responsibilities definition
+
+CREATE TABLE `b3_operational_development_responsibilities` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `commitee` varchar(255) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `b3_operational_development_responsibilities_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.b4_professional_experiances_internal definition
+
+CREATE TABLE `b4_professional_experiances_internal` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `b4_professional_experiances_internal_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.b5_professional_experiances_national definition
+
+CREATE TABLE `b5_professional_experiances_national` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `b5_professional_experiances_national_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.c1_lead_new_research definition
+
+CREATE TABLE `c1_lead_new_research` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `c1_lead_new_research_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.c2_research_development_projects definition
+
+CREATE TABLE `c2_research_development_projects` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `lead_or_co` tinyint(1) NOT NULL,
+  `internal_or_external` tinyint(1) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `c2_research_development_projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.c3_research_development_operations definition
+
+CREATE TABLE `c3_research_development_operations` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `lead_or_co` tinyint(1) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `c3_research_development_operations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.d_professional_consultations definition
+
+CREATE TABLE `d_professional_consultations` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `monetary` tinyint(1) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `d_professional_consultations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- mirosdb.g_services_to_community definition
+
+CREATE TABLE `g_services_to_community` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `institute` tinyint(1) NOT NULL,
+  `district` tinyint(1) NOT NULL,
+  `state` tinyint(1) NOT NULL,
+  `national` tinyint(1) NOT NULL,
+  `international` tinyint(1) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `g_services_to_community_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
