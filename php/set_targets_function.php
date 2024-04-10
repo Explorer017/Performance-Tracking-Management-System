c5ca7e4204cfcfc8145593ee17d3cb3452398de3
Finn ONeill <56732347+Explorer017@users.noreply.github.com>
2024-04-10 15:29:29 +0100

    Update set_targets_function.php

--- php/set_targets_function.php
+++ php/set_targets_function.php
@@ -6,7 +6,7 @@ function set_targets($section_number, $year, $target_amount){
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
     }
-    $sql = "INSERT INTO targets (section_number, year, target_amount) VALUES ('$section_number', '$year', '$target_amount')";
+    $sql = "INSERT INTO targets (section_number, year, target) VALUES ('$section_number', '$year', '$target_amount')";
     if ($db->query($sql) === TRUE) {
         return true;
     }
