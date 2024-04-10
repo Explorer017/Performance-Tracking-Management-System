357295f43e3d8e8e7af0c7776569e0e5eebfcfd4
Finn ONeill <56732347+Explorer017@users.noreply.github.com>
2024-04-10 16:23:01 +0100

    Create edit_user.php

---
+++ php/edit_user.php
@@ -0,0 +1,92 @@
+<?php
+    $english = true;
+    include 'navbar.php';
+
+    $userid = $_GET['userid'];
+
+    $valid_form = true;
+    $first_name = $middle_name = $last_name = $access_level = $email = "";
+    $first_name_error = $middle_name_error = $last_name_error = $access_level_error = $email_error = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["first_name"])) {
+            $valid_form = false;
+            $first_name_error = "First Name is required";
+        } else {
+            $first_name = htmlspecialchars($_POST["first_name"]);
+        }
+        if (empty($_POST["middle_name"])) {
+            $valid_form = false;
+            $middle_name_error = "Middle Name is required";
+        } else {
+            $middle_name = htmlspecialchars($_POST["middle_name"]);
+        }
+        if (empty($_POST["last_name"])) {
+            $valid_form = false;
+            $last_name_error = "Last Name is required";
+        } else {
+            $last_name = htmlspecialchars($_POST["last_name"]);
+        }
+        if (empty($_POST["access_level"])) {
+            $valid_form = false;
+            $access_level_error = "Access Level is required";
+        } else {
+            $access_level = htmlspecialchars($_POST["access_level"]);
+        }
+    }
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href= "../css/style.css">
+    <title>Set targets</title>
+</head>
+<?php if (isset($userid)):?>
+    <body>
+    <div class='container'>
+        <h1>Modify a user: </h1>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <div class="mb-3">
+                <label for="first_name" class="form-label">First Name: </label>
+                <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name" value=""/>
+                <div><?php echo $first_name_error?></div>
+            </div>
+            <div class="mb-3">
+                <label for="middle_name" class="form-label">Middle Name: </label>
+                <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Middle Name" value=""/>
+                <div><?php echo $middle_name_error?></div>
+            </div>
+            <div class="mb-3">
+                <label for="last_name" class="form-label">Last Name: </label>
+                <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name" value=""/>
+                <div><?php echo $last_name_error?></div>
+            </div>
+            <div class="mb-3">
+                <label for="access_level" class="form-label">Access Level: </label>
+                <select class="form-select" id="access_level" name="access_level">
+                    <option value="0">Research Officer</option>
+                    <option value="1">Supervisor</option>
+                    <option value="2">Manager</option>
+                    <option value="3">Admin</option>
+                </select>
+                <div><?php echo $access_level_error?></div>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email: </label>
+                <input type="text" class="form-control" id="email" name="email" placeholder="Email" value=""/>
+                <div><?php echo $email_error?></div>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    </body>
+<?php else:?>
+    <body>
+    <div class="container">
+        no user
+    </div>
+    </body>
+<?php endif;?>
