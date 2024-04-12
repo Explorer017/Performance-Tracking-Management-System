e96260567ed6063f799ac43177a26d3d292651ca
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2024-04-12 20:44:24 +0100

    deleted duplicate "D" table

--- php/submissionPage.php
+++ php/submissionPage.php
@@ -101,7 +101,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <option value="G">G: Services to Community</option>
                 </select>
                 <input class="btn-primary" type="submit" value="Select Section" name="submit">
-                <div><?php echo $section_number_error?></div>
+                <!--<div><?php echo $section_number_error?></div>-->
             </div>
         </form>
 
@@ -535,55 +535,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </main>
 </div>
 <?php endif;?>
-
-
-<?php if ($table == "D"):?>
-<div class="container bgColor">
-    <main role="main" class="pb-3">
-        <div class="row">
-            <div class="col-8">
-                <!--<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">-->
-                <form action="submissionPage.php" method="post">
-                    <input type='hidden' name='table' value='D'/>
-                    <div class="form-group col-md-6">
-                        <label class="control-label labelFont">User ID</label>
-                        <input class="form-control" placeholder="Enter the user ID" type="text" name="userID">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label class="control-label labelFont">Monetary</label>
-                        <select name="monetary" class="form-control" required>
-                            <option value="1">Yes</option>
-                            <option value="0">No</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label class="control-label labelFont">Section Number</label>
-                        <input class="form-control" placeholder="Enter the section number" type="text" name="sectionNumber">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label class="control-label labelFont">Year</label>
-                        <input class="form-control" placeholder="Enter the year" type="text" name="yearOfUpload">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label class="control-label labelFont">Supporting File ID</label>
-                        <input class="form-control" placeholder="Enter the supporting file ID" type="text" name="supportingFileID">
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label class="control-label labelFont">Points</label>
-                        <input class="form-control" placeholder="Enter the points given" type="text" name="points">
-                    </div>
-                    
-                    <div class="form-group col-md-4">
-                        <input class="btn-primary" type="submit" value="Finish Submission" name="submit">
-                    </div>
-                </form>
-            </div>
-        </div>
-    </main>
-</div>
-<?php endif;?>
-
-
+    
 <?php if ($table == "E3-4-13"):?>
 <div class="container bgColor">
     <main role="main" class="pb-3">
